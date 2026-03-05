80d7b780f962e026a82d49da7f6803dab46daa6a
guoshuai <406601626@qq.com>
2026-03-05 20:22:31 +0800

    初始化

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,8 +4,15 @@
 
 <template>
   <div>this is index page</div>
-  <div class="[--gutter-width:1rem] lg:[--gutter-width:2rem] text-(length:--gutter-width)">111</div>
-  <nuxt-link to="/product">to product</nuxt-link>
+  <div>
+    <div class="group">
+      <div class="group-even:bg-red-400">111</div>
+    </div>
+    <div class="group">
+      <div class="group-even:bg-red-400">222</div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui'],
+  // modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   ignore: [
     'app/pages/**/components/**' // 明确指定忽略 pages 下组件文件夹里的 vue 文件
@@ -11,5 +13,10 @@ export default defineNuxtConfig({
   //   '/api/**': {
   //     proxy: 'http://192.168.9.147:8897/stage-api/**'
   //   }
-  // }
+  // },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
